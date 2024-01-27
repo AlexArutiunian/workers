6d24fd7f29852243a77c098341ab7e101a848ebe
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-01-27 17:10:41 +0300

    upd datas

--- del_no_human.py
+++ del_no_human.py
@@ -1,7 +1,7 @@
 import json
 
 def remove_items_with_link_for_arts(json_data):
-    return [item for item in json_data if item.get('link_for_arts', '') != '1']
+    return [item for item in json_data if item.get('occupations', '') != '1']
 
 json_file_path = 'workers_/data.json'
 
